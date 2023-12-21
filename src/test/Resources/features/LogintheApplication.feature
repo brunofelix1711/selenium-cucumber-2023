121b72d72328f51e7dedb9b9635fe02bf56b7d7d
brunofelix1711 <arptest2302@gmail.com>
2023-12-21 17:38:00 +0530

    stepdefinition class is added

--- src/test/Resources/features/LogintheApplication.feature
+++ src/test/Resources/features/LogintheApplication.feature
@@ -1,2 +1,7 @@
+@functional
 Feature: To Login the Application
   Scenario: To login the application with username and password
+
+    Given When I Launch the application
+    Then I enter the "username" and "Password"
+    Then I click submit button
---
+++ src/test/java/com/stepdefinition/LoginStepDefinition.java
@@ -0,0 +1,24 @@
+package com.stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class LoginStepDefinition {
+
+    @Given("When I Launch the application")
+    public void when_i_launch_the_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I enter the {string} and {string}")
+    public void i_enter_the_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I click submit button")
+    public void i_click_submit_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
